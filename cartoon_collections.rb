281c9ac00ec39a53c273b9cb6999d7812eb4b584
john L <johnleavell@gmail.com>
2019-11-16 13:22:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(names)# code an argument here
   names.each_with_index do |names, index|
     puts "#{index + 1}. #{name}"
   end
-  
+
 end
 
 def summon_captain_planet# code an argument here
