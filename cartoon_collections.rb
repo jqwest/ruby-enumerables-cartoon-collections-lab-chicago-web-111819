d9aaa3bfb8bff1ab76e9e56a6a98bbfd5d172746
john L <johnleavell@gmail.com>
2019-11-16 13:31:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map { |call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
